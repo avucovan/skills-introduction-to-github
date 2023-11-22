a1bacbba2a9ff1d0c3f6a2a4e0cd4d4a2cb0420f
Adrian Vucovan <adrian.vucovan@icloud.com>
2023-11-21 21:05:17 -0500

    20231121-3

--- Projects/1st C program.c
+++ Projects/1st C program.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 int main ()
 {
-    int number;
+    int number1, number2, total;
     printf ("Just a small step for coders. One giant leap for");
     printf ("programmers!\n");
     printf ("I am learning the C programming language\n");
-    printf ("Enter an integer ");
-    scanf (" %d", &number);
-    printf ("You entered: %d", number );
+    printf ("Enter first integer:");
+    scanf (" %d", &number1);
+    printf ("Emter second integer: ");
+    scanf (" %d", &number2);
+    total = number1 + number2;
+    printf ("%d + %d = %d", number1, number2, total);
+    printf("\n");
     return 0;
 }
