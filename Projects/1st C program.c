1f295e3f32f0c00061cd4a3f422bb5d8b9a28559
Adrian Vucovan <adrian.vucovan@icloud.com>
2023-11-21 20:24:04 -0500

    changes 20231121

--- Projects/1st C program.c
+++ Projects/1st C program.c
@@ -1,8 +1,13 @@
 /* Prints a message on the screen */
 #include <stdio.h>
-main ()
+int main ()
 {
+    int number;
     printf ("Just a small step for coders. One giant leap for");
     printf ("programmers!\n");
+    printf ("I am learning the C programming language\n");
+    printf ("Enter an integer ");
+    scanf (" %d", &number);
+    printf ("You entered: %d", number );
     return 0;
 }
