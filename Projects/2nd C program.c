7d434967a15e20a477f99687a20d2b495273d8fa
Adrian Vucovan <adrian.vucovan@icloud.com>
2023-11-22 21:32:56 -0500

    Commit 20231122

---
+++ Projects/2nd C program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main ()
+{
+FILE *fp;
+char ch;
+fp = fopen("hello.txt", "w");
+printf ("Enter data:\n");
+while ((ch = getchar()) != EOF) {
+    putc (ch,fp);
+}
+fclose(fp);
+fp = fopen("hello.txt", "r");
+
+while( (ch = getc(fp) != EOF))
+    printf("%c", ch);
+
+fclose(fp);
+}
